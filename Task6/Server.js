9be0a750ac39635551803a2b68be7f6b62d505a0
Mihnea - Alexandru Mihaila <78567562+MihneaMihaila@users.noreply.github.com>
2021-05-11 15:51:09 +0300

    Update Server.js

--- Task6/Server.js
+++ Task6/Server.js
@@ -12,6 +12,12 @@ app.get('/', (req, res) =>{
  password:"mihnea" ,
  database:"database" 
 }); */
+ var con = mysql.createConnection({
+ host:process.env.host ,
+ user:process.env.user ,
+ password:process.env.password ,
+ database:process.env.database 
+}); 
 con.connect(function(err) {
  if (err) throw err;
  console.log("Connected!");
